bce6df9c43ae0f9d8364b2d3b4c31a85be66f9cf
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2026-03-06 16:32:53 +0700

    feaT: add zoxide navigation.

--- init.lua
+++ init.lua
@@ -3,34 +3,33 @@ require("session"):setup {
 }
 require("git"):setup()
 
-
 -- require("gvfs"):setup({ })
 -- You can configure your bookmarks using simplified syntax
-local bookmarks = {
-  { tag = "Desktop", path = "~/Desktop", key = "d" },
-  { tag = "Documents", path = "~/Documents", key = "D" },
-  { tag = "Downloads", path = "~/Downloads", key = "o" },
-}
+-- local bookmarks = {
+--   { tag = "Desktop", path = "~/Desktop", key = "d" },
+--   { tag = "Documents", path = "~/Documents", key = "D" },
+--   { tag = "Downloads", path = "~/Downloads", key = "o" },
+-- }
 
--- You can also configure bookmarks with key arrays
-local bookmarks = {
-  { tag = "Desktop", path = "~/Desktop", key = { "d", "D" } },
-  { tag = "Documents", path = "~/Documents", key = { "d", "d" } },
-  { tag = "Downloads", path = "~/Downloads", key = "o" },
-}
+-- -- You can also configure bookmarks with key arrays
+-- local bookmarks = {
+--   { tag = "Desktop", path = "~/Desktop", key = { "d", "D" } },
+--   { tag = "Documents", path = "~/Documents", key = { "d", "d" } },
+--   { tag = "Downloads", path = "~/Downloads", key = "o" },
+-- }
 
--- Windows-specific bookmarks
-if ya.target_family() == "windows" then
-  local home_path = os.getenv("USERPROFILE")
-  table.insert(bookmarks, {
-    tag = "Scoop Local",
-    path = os.getenv("SCOOP") or (home_path .. "\\scoop"),
-    key = "p"
-  })
-  table.insert(bookmarks, {
-    tag = "Scoop Global",
-    path = os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop",
-    key = "P"
-  })
-end
+-- -- Windows-specific bookmarks
+-- if ya.target_family() == "windows" then
+--   local home_path = os.getenv("USERPROFILE")
+--   table.insert(bookmarks, {
+--     tag = "Scoop Local",
+--     path = os.getenv("SCOOP") or (home_path .. "\\scoop"),
+--     key = "p"
+--   })
+--   table.insert(bookmarks, {
+--     tag = "Scoop Global",
+--     path = os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop",
+--     key = "P"
+--   })
+-- end
 
